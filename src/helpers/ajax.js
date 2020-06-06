83e60cb7e21c91ace453ca7dd90679420cdb12d9
Patrick Lyden <syncc12@verizon.net>
2020-06-06 16:25:43 +0000

    Updated ajax.js ajaxPath to point to the proper url in production.

--- src/helpers/ajax.js
+++ src/helpers/ajax.js
@@ -3,8 +3,8 @@ const ajaxPath = (endpoint) => {
     console.log('endpoint','http://localhost:8080/');
     return 'http://localhost:8080/' + endpoint;
   } else {
-    console.log('endpoint','https://jobsearch-react.herokuapp.com/');
-    return 'https://jobsearch-react.herokuapp.com/' + endpoint;
+    console.log('endpoint','https://jobsearch-react-api.herokuapp.com/');
+    return 'https://jobsearch-react-api.herokuapp.com/' + endpoint;
   }
     
 };
